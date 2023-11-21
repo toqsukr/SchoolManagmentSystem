041bf3917066483e2b0a26f9245e3d213299b033
Wastard <Ilya.pakylov@gmail.com>
2023-11-21 18:36:16 +0300

    completed addteacher window graphics

--- src/main/java/application/graphic/AddTeacherGUI.java
+++ src/main/java/application/graphic/AddTeacherGUI.java
@@ -1,122 +1,74 @@
 package application.graphic;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Container;
-import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.util.List;
 
-import javax.swing.Box;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import application.entities.Klass;
 import application.entities.Subject;
 import application.graphic.ui.CheckBoxList;
 
 public class AddTeacherGUI extends AddGUI {
 
-    private static final JTextField inputNameField = new JTextField("", 15);
+    private final JTextField inputNameField = new JTextField(20);
 
-    private static final JTextField inputSurnameField = new JTextField("", 15);
+    private final JTextField inputSurnameField = new JTextField( 20);
 
-    private final CheckBoxList<Subject> subjectRadio;
+    private final CheckBoxList<Subject> subjectCheckBox;
 
-    private static final JButton addBtn = new JButton("Добавить");
+    private final CheckBoxList<Klass> klassCheckBox;
 
-    private static final JButton cancelBtn = new JButton("Отмена");
+    private final JButton addBtn = new JButton("Добавить");
 
-    private static final JLabel nameLabel = new JLabel("Имя:");
+    private final JButton cancelBtn = new JButton("Отмена");
 
-    private static final JLabel surnameLabel = new JLabel("Фамилия:");
+    private final JLabel nameLabel = new JLabel("Имя:");
 
-    private static final JLabel subjectLabel = new JLabel("Предмет:");
+    private final JLabel surnameLabel = new JLabel("Фамилия:");
+
+    private final JLabel subjectLabel = new JLabel("Предмет:");
+
+    private final JLabel klassLabel = new JLabel("Класс:");
     
   public AddTeacherGUI() {
     super();
 
     List<Subject> subjects = Subject.getEntityDao().getAll();
-    subjectRadio = new CheckBoxList<>(subjects);
+    subjectCheckBox = new CheckBoxList<>(subjects);
 
-    subjectRadio.setBackground(new Color(0xFFFFFF, false));
-    subjectRadio.setFocusable(false);
+    List<Klass> klasses = Klass.getEntityDao().getAll();
+    klassCheckBox = new CheckBoxList<>(klasses);
+  
+    subjectCheckBox.setBackground(new Color(0xFFFFFF, false));
+    subjectCheckBox.setFocusable(false);
     addBtn.setBackground(new Color(0xDFD9D9D9, false));
     cancelBtn.setBackground(new Color(0xDFD9D9D9, false));
 
-    Box leftBox = Box.createVerticalBox();
-    Box centerBox = Box.createVerticalBox();
-    Box rightBox = Box.createVerticalBox();
+    JPanel panel = new JPanel();
+    panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+    panel.setLayout(new GridLayout(5, 2, 10, 10));
 
-    Box toolBox = Box.createHorizontalBox();
+    panel.add(nameLabel);
+    panel.add(inputNameField);
 
-    Box nameBox = Box.createHorizontalBox();
-    Box surnameBox = Box.createHorizontalBox();
-    Box klassBox = Box.createHorizontalBox();
-    Box entityBox = Box.createHorizontalBox();
+    panel.add(surnameLabel);
+    panel.add(inputSurnameField);
 
-    addBtn.setFocusable(false);
+    panel.add(subjectLabel);
+    panel.add(subjectCheckBox);
 
-    cancelBtn.setFocusable(false);
+    panel.add(klassLabel);
+    panel.add(klassCheckBox);
 
-    Container container = this.getContentPane();
-    container.setLayout(new BorderLayout());
+    panel.add(addBtn);
+    panel.add(cancelBtn);
 
-    toolBox.add(Box.createRigidArea(new Dimension(40, 0)));
-    toolBox.add(addBtn);
-    toolBox.add(Box.createRigidArea(new Dimension(20, 0)));
-    toolBox.add(cancelBtn);
-
-    centerBox.add(Box.createRigidArea(new Dimension(20, 45)));
-
-    nameBox.add(Box.createRigidArea(new Dimension(45, 0)));
-    nameBox.add(nameLabel);
-    nameBox.add(Box.createRigidArea(new Dimension(20, 0)));
-    nameBox.add(inputNameField);
-    centerBox.add(nameBox);
-    centerBox.add(Box.createRigidArea(new Dimension(0, 20)));
-
-    klassBox.add(Box.createRigidArea(new Dimension(45, 0)));
-    klassBox.add(subjectLabel);
-    klassBox.add(Box.createRigidArea(new Dimension(51, 0)));
-    klassBox.add(subjectRadio);
-    centerBox.add(klassBox);
-    centerBox.add(Box.createRigidArea(new Dimension(0, 10)));
-
-    surnameBox.add(Box.createRigidArea(new Dimension(15, 0)));
-    Box surnameLabelBox = Box.createHorizontalBox();
-    surnameLabelBox.add(Box.createRigidArea(new Dimension(30, 0)));
-    surnameLabelBox.add(surnameLabel);
-    surnameLabelBox.add(Box.createRigidArea(new Dimension(30, 0)));
-
-    Box teamInnerBox = Box.createVerticalBox();
-    teamInnerBox.add(surnameLabelBox);
-    surnameBox.add(teamInnerBox);
-    surnameBox.add(Box.createRigidArea(new Dimension(16, 0)));
-
-    Box teamInputBox = Box.createVerticalBox();
-
-    teamInputBox.add(Box.createRigidArea(new Dimension(0, 10)));
-    teamInputBox.add(inputNameField);
-    teamInputBox.add(nameLabel);
-    teamInputBox.add(Box.createRigidArea(new Dimension(0, 10)));
-    surnameBox.add(teamInputBox);
-    centerBox.add(surnameBox);
-    centerBox.add(Box.createRigidArea(new Dimension(0, 15)));
-
-    entityBox.add(Box.createRigidArea(new Dimension(45, 0)));
-    entityBox.add(surnameLabel);
-    entityBox.add(Box.createRigidArea(new Dimension(67, 0)));
-    entityBox.add(inputSurnameField);
-
-    centerBox.add(entityBox);
-    centerBox.add(Box.createRigidArea(new Dimension(0, 20)));
-    centerBox.add(toolBox);
-    centerBox.add(Box.createRigidArea(new Dimension(0, 50)));
-
-    rightBox.add(Box.createRigidArea(new Dimension(45, 0)));
-
-    container.add(leftBox, BorderLayout.WEST);
-    container.add(centerBox, BorderLayout.CENTER);
-    container.add(rightBox, BorderLayout.EAST);
+    add(panel);
   }
 }
